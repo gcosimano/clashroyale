38f989fdf7398da36cd70888892b6ed3654c7747
bd552801 <bd552801@ahschool.com>
2025-10-03 12:30:55 -0400

    card classes -- fixing for loop to make deck
    Co-authored-by: giannacosimano08@gmail.com <giannacosimano08@gmail.com>

--- Card.py
+++ Card.py
@@ -1,11 +1,11 @@
+
 class Card:
     
-    def __init__(self, sc_key, elixir, type, combat_stats, damage, mechanics, counters, synergies):
+    def __init__(self, sc_key, elixir, type, combat_stats, mechanics, counters, synergies):
         self.sc_key = sc_key
         self.elixir = elixir
         self.type = type
         self.combat_stats = combat_stats
-        self.damage = damage
         self.mechanic = mechanics
         self.counters = counters
-        self.synergies = synergies
+        self.synergies = synergies    
--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import get_data
+import Card
 import pprint
 
 troops = ['Musketeer', 'Knight', 'Archers', 'Giant', 'Minions', 'Mini Pekka', 'Spear Goblins', 'Goblins', 'Goblin Cage']
@@ -19,18 +20,21 @@ else:
 # making Decks with Card cards
 d = get_data.get_deck('data/', troops, spells)
 deck = []
-for i in range(12):
-    sc_key = d[i["sc_key"]]
-    type = d[i["type"]]
-    elixir = d[i["elixir"]]
-    combat = d[i["combat_stats"]]
-    damage = d[i["damage"]]
-    mechanics = d[i["mechanics"]]
-    synergies = d[i["synergies"]]
-    counters = d[i["counters"]]
-    d.append(Card(sc_key, elixir, type, combat, damage, mechanics, counters, synergies))
+for i in range(len(d)-1):
+    print(i)
+    sc_key = d[i]["sc_key"]
+    type = d[i]["type"]
+    elixir = d[i]["elixir"]
+    combat = d[i]["combat_stats"]
+    mechanics = d[i]["mechanics"]
+    synergies = d[i]["synergies"]
+    counters = d[i]["counters"]
+    a = Card.Card(sc_key, elixir, type, combat, mechanics, counters, synergies, 1)
+    deck.append(a)
 
-print(deck)
+# print(deck)
+print(deck[0])
+print(deck[0]['sc_key'])
 
 
 
